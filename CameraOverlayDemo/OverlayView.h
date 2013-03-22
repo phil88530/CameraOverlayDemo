3a73ba277a17a1c49923273c3d609a59e85fed02
YuFei Zhu <Phil@mohcs-mac-mini.internal.mohc.net>
2013-03-22 14:22:46 +0000

    first commit

---
+++ CameraOverlayDemo/OverlayView.h
@@ -0,0 +1,13 @@
+//
+//  OverlayView.h
+//  CameraOverlayDemo
+//
+//  Created by YuFei Zhu on 22/03/2013.
+//  Copyright (c) 2013 YuFei Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OverlayView : UIView
+
+@end
---
+++ CameraOverlayDemo/OverlayView.m
@@ -0,0 +1,31 @@
+//
+//  OverlayView.m
+//  CameraOverlayDemo
+//
+//  Created by YuFei Zhu on 22/03/2013.
+//  Copyright (c) 2013 YuFei Zhu. All rights reserved.
+//
+
+#import "OverlayView.h"
+
+@implementation OverlayView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
